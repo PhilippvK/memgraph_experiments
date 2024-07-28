507365e1d50d9efb345bcd9f8e37618ed0b2f47e
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-07-28 20:29:47 +0200

    add properties to index file

--- tool/index.py
+++ tool/index.py
@@ -5,9 +5,14 @@ import yaml
 import pandas as pd
 
 
-def write_index_file(dest: Union[str, Path], subs_df: pd.DataFrame, index_data: Dict[int, Dict[str, Any]]):
+def write_index_file(
+    dest: Union[str, Path],
+    subs_df: pd.DataFrame,
+    index_data: Dict[int, Dict[str, Any]],
+    include_properties: bool = True,
+):
     yaml_data = {"candidates": []}
-    for sub_id in subs_df.index:
+    for sub_id, row in subs_df.iterrows():
         assert sub_id in index_data
 
         def helper(x):
@@ -17,6 +22,19 @@ def write_index_file(dest: Union[str, Path], subs_df: pd.DataFrame, index_data: 
 
         artifacts_data = {key: helper(value) for key, value in index_data[sub_id].items()}
         new = {"id": sub_id, "artifacts": artifacts_data}
+
+        if include_properties:
+
+            def fix_types(x):
+                if isinstance(x, (int, float, str, list)):
+                    return x
+                if isinstance(x, (set, tuple)):
+                    return list(x)
+                assert NotImplementedError("Unsupported Type: {type(x)}")
+
+            row_data = {key: fix_types(value) for key, value in row.to_dict().items()}
+            # print("row_data", row_data)
+            new["properties"] = row_data
         yaml_data["candidates"].append(new)
     with open(dest, "w") as f:
         yaml.dump(yaml_data, f, default_flow_style=False)
