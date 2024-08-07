8812d88ba6eae2e178e748eda98892bfda16959f
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-08-07 19:33:06 +0200

    updates

--- tool/gen/desc.py
+++ tool/gen/desc.py
@@ -1,7 +1,8 @@
 def generate_desc(i, sub_data, name="result"):
+    dt = sub_data["DateTime"]
     # inputs = sub_data["InputNodes"]
     num_inputs = int(sub_data["#InputNodes"])
     # outputs = sub_data["OutputNodes"]
     num_outputs = int(sub_data["#OutputNodes"])
-    desc = f"Sub: {i}, Name: {name}, Inputs: {num_inputs}, Outputs: {num_outputs}"
+    desc = f"DateTiome: {dt}, Sub: {i}, Name: {name}, Inputs: {num_inputs}, Outputs: {num_outputs}"
     return desc
