4fe1b79aae57fec02b281ee02993733e8d809f94
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-08-09 12:02:53 +0200

    tool/gen/desc.py: fix typo

--- tool/gen/desc.py
+++ tool/gen/desc.py
@@ -4,5 +4,5 @@ def generate_desc(i, sub_data, name="result"):
     num_inputs = int(sub_data["#InputNodes"])
     # outputs = sub_data["OutputNodes"]
     num_outputs = int(sub_data["#OutputNodes"])
-    desc = f"DateTiome: {dt}, Sub: {i}, Name: {name}, Inputs: {num_inputs}, Outputs: {num_outputs}"
+    desc = f"DateTime: {dt}, Sub: {i}, Name: {name}, Inputs: {num_inputs}, Outputs: {num_outputs}"
     return desc
