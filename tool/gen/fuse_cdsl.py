214f602b9d1f5b5a9c3f79b2f6dfecf0d39505c5
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-10-02 10:15:48 +0200

    implement tool/gen/fuse_cdsl.py

--- tool/gen/fuse_cdsl.py
+++ tool/gen/fuse_cdsl.py
@@ -7,112 +7,22 @@ import yaml
 import pandas as pd
 from tqdm import tqdm
 
-from .cdsl_utils import CDSLEmitter, wrap_cdsl
+from .flat_utils import FlatCodeEmitter
 from .tree_utils import tree_from_pkl
 from .desc import generate_desc
+from .cdsl import generate_operands, generate_complete_cdsl
 
-logger = logging.getLogger("cdsl")
-
-
-def generate_operands(sub_data: dict, xlen: int, with_attrs: bool = True):
-    operands = {}
-    operand_names = sub_data["OperandNames"]
-    # operand_nodes = sub_data["OperandNodes"]
-    operand_dirs = sub_data["OperandDirs"]
-    operand_types = sub_data["OperandTypes"]  # TODO: rename to Classes?
-    operand_enc_bits = sub_data["OperandEncBits"]
-    operand_reg_classes = sub_data["OperandRegClasses"]
-    for i, operand_name in enumerate(operand_names):
-        operand_dir = operand_dirs[i]
-        operand_type = operand_types[i]
-        operand_reg_class = operand_reg_classes[i]
-        is_output = operand_dir in ["OUT", "INOUT"]
-        enc_bits = operand_enc_bits[i]
-        if operand_type == "REG":
-            if operand_reg_class == "GPR":
-                operand_code = f"unsigned<{enc_bits}> {operand_name}"
-                if with_attrs:
-                    attrs = {
-                        "is_reg": None,
-                        "reg_class": operand_reg_class.upper(),
-                        "reg_type": f"s{xlen}",
-                        operand_dir.lower(): None,
-                    }
-
-                    def convert_val(x):
-                        if isinstance(x, str):
-                            return f'"{x}"'
-                        return x
-
-                    attr_strs = [
-                        f"[[{key}={convert_val(value)}]]" if value is not None else f"[[{key}]]"
-                        for key, value in attrs.items()
-                    ]
-                    attrs_str = " ".join(attr_strs)
-                    operand_code += " " + attrs_str
-            else:
-                raise NotImplementedError
-        else:
-            raise NotImplementedError
-
-        operand = (operand_code, operand_reg_class.lower(), is_output)
-        operands[operand_name] = operand
-    return operands
+logger = logging.getLogger("fuse_cdsl")
 
 
-def generate_complete_cdsl(codes: List[str], operands, xlen: int, name="result", desc=None):
-    # print("CDSL Code:")
-    codes = ["    " + code for code in codes]
-    asm_ins = []
-    asm_outs = []
-    mnemonic = "myinst"
-    operands_code = "operands: {\n"
-    for operand_name, operand in operands.items():
-        operand_code, reg_class, is_output = operand
-        asm_str = operand_name
-        if reg_class == "gpr":
-            asm_str = f"name({operand_name})"
-        elif reg_class == "gpr":
-            asm_str = f"fname({operand_name})"
-        asm_str = f"{{{asm_str}}}"
-        if is_output:
-            asm_outs.append(asm_str)
-        else:
-            asm_ins.append(asm_str)
-        operands_code += "    " + operand_code + ";\n"
-    asm_all = asm_outs + asm_ins
-    asm_syntax = ", ".join(asm_all)
-    operands_code += "}"
-    codes = (
-        [operands_code, "encoding: auto;", f'assembly: {{"{mnemonic}", "{asm_syntax}"}};', "behavior: {"]
-        + codes
-        + ["}"]
-    )
-    codes = wrap_cdsl("MyInst", "\n".join(codes)).split("\n")
-    codes = ["    " * 2 + code for code in codes]
-    code = "\n".join(codes) + "\n"
-    prefix = f'import "RV{xlen}I.core_desc"\n\n'
-    code = (
-        prefix
-        + f"""InstructionSet MySet extends RV{xlen}I {{
-    instructions {{
-{code}
-    }}
-}}
-"""
-    )
-    if desc:
-        code = f"// {desc}\n\n" + code
-    return code.strip()
-
-
-def generate_cdsl(stmts_root, sub_data, xlen: int, name="result", desc=None):
-    emitter = CDSLEmitter(xlen)
+def generate_fuse_cdsl(stmts_root, sub_data, xlen: int, name="result", desc=None):
+    codes = []
+    emitter = FlatCodeEmitter()
     emitter.visit(stmts_root)
     output = emitter.output
-    codes = []
     codes += output.split("\n")
     operands = generate_operands(sub_data, xlen=xlen, with_attrs=True)
+
     return generate_complete_cdsl(codes, operands=operands, xlen=xlen, name=name, desc=desc)
 
 
@@ -164,10 +74,10 @@ def process(
         stmts = tree_from_pkl(tree_pkl)
         # print("stmts", stmts)
         # TODO: make sure that result/sub col in combined index is unique
-        cdsl_code = generate_cdsl(stmts, sub_data, xlen=xlen, name=name, desc=desc)
+        cdsl_code = generate_fuse_cdsl(stmts, sub_data, xlen=xlen, name=name, desc=desc)
         print("cdsl_code")
         print(cdsl_code)
-        with open(out_dir / f"{name}.core_desc", "w") as f:
+        with open(out_dir / f"{name}.fuse_core_desc", "w") as f:
             f.write(cdsl_code)
         candidate_artifacts["cdsl"] = str(out_dir / f"{name}.core_desc")
         yaml_data["candidates"][i]["artifacts"] = candidate_artifacts
