f6dc18071d488ea922e3b671f4d68b27b28804ef
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-02-17 11:29:01 +0100

    Python3.12 enum fix

--- tool/stages/detect_predicates.py
+++ tool/stages/detect_predicates.py
@@ -10,7 +10,7 @@ logger = logging.getLogger("detect_predicates")
 
 def detect_predicates(settings, subs, subs_df, io_isos):
     logger.info("Detecting Predicates...")
-    subs_df["Predicates"] = InstrPredicate.NONE
+    subs_df["Predicates"] = InstrPredicate.NONE.value
     subs_iter = [(i, sub) for i, sub in enumerate(subs) if i not in io_isos]
     # for i, sub in enumerate(tqdm(subs, disable=not settings.progress)):
     for i, sub in tqdm(subs_iter, disable=not settings.progress):
